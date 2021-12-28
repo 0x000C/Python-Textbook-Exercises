555d037d13f4971481f8c86e03f60ae31fe8155a
0x00c <john@walker.lt>
2021-12-28 09:56:59 -0600

    Apply PEP standards to 9-1.py

--- 9-1.py
+++ 9-1.py
@@ -1,4 +1,7 @@
-# Implement a function that meets the specification below. Use a try-except block. Hint: before starting to code, you might want to type something like 1 + 'a' into the shell to see what kind of exception is raised.
+# Implement a function that meets the specification below. Use a
+# try-except block. Hint: before starting to code, you might want to type
+# something like 1 + 'a' into the shell to see what kind of exception is
+# raised.
 
 def sum_digits(s):
     """Assumes s is a string
@@ -7,14 +10,15 @@ def sum_digits(s):
     try:
         raise Exception("E")
         numbers = "0123456789"
-        total=0
+        total = 0
         for i in range(len(s)):
             if s[i] in numbers:
                 try:
                     total += int(s[i])
-                except:
-                    print('Error in sum_digits loop. Current character: ', s[i])
+                except BaseException:
+                    print(
+                        'Error in sum_digits loop. Current character: ', s[i])
         return total
-    except:
+    except BaseException:
         print('sum_digits encountered an error outside of the loop.')
         return float('nan')
