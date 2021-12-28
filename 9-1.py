d21bf92dfbab09d3a18537ab2b2a83f7b032512e
0x00c <john@walker.lt>
2021-12-28 09:57:56 -0600

    Remove exception testing line

--- 9-1.py
+++ 9-1.py
@@ -8,7 +8,6 @@ def sum_digits(s):
        Returns the sum of the decimal digits in s
           For example, if s is 'a2b3c' it returns 5"""
     try:
-        raise Exception("E")
         numbers = "0123456789"
         total = 0
         for i in range(len(s)):
