77db427a310c48c60bbf828ffb31a7617957a81e
0x000C <john@walker.lt>
2022-01-09 10:28:37 -0600

    Add exercise 10-3

---
+++ 10-3.py
@@ -0,0 +1,26 @@
+# Implement a subclass of Person that meets the specification
+
+
+class Politician(Person):
+    """ A politician is a person who can belong to a political party"""
+
+    def __init__(self, name, party=None):
+        """name and party are strings"""
+        super().__init__(name)
+        self._party = party
+
+    def get_party(self):
+        """returns the party to which self belongs"""
+        return self._party
+
+    def might_agree(self, other):
+        """returns True if self and other belong to the same part
+             or at least one of then does not belong to a party"""
+        if self.get_party() is None:
+            return True
+        elif other.get_party() is None:
+            return True
+        elif self.get_party() == other.get_party():
+            return True
+        else:
+            return False
