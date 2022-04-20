e97c2ed47569e23514fa815e42200df7ed8a89ff
0x000C <44306472+0x000C@users.noreply.github.com>
2022-04-19 19:33:06 -0500

    Add files via upload

---
+++ 21-3.py
@@ -0,0 +1,9 @@
+# Finger exercise: You are wandering through a forest and see a field of delicious-looking mushrooms.
+# You fill your basket with them, and head home prepared to cook them up and serve them to your
+# husband. Before you cook them, however, he demands that you consult a book about local mushroom
+# species to check whether they are poisonous. The book says that 80% of the mushrooms in the local
+# forest are poisonous. However, you compare your mushrooms to the ones pictured in the book, and
+# decide that you are 95% certain that your mushrooms are safe. How comfortable should you be about
+# serving them to your husband (assuming that you would rather not become a widow)?
+
+print((0.95*0.2)/((0.95*0.2)+(0.05*0.95)))
